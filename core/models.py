355c187ced93c450d319c32e171b431d3d99d389
Alice Campkin <alice.campkin@gmail.com>
2022-07-17 12:37:56 +0100

    adds todo model

---
+++ core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDo(models.Model):
+
+    STATUS_CHOICES = [
+        ("todo", "To Do"),
+        ("doing", "Doing"),
+        ("done", "Done"),
+        ("abandoned", "Abandoned")
+    ]
+
+    name = models.CharField(max_length=255)
+    status = models.CharField(choices=STATUS_CHOICES, default="todo", max_length=255)
+
+    def __str__(self):
+        return self.name
+    
