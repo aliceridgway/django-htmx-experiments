141835f3f1075638590a859847ac71ee31c164b2
Alice Campkin <alice.campkin@gmail.com>
2022-07-17 15:27:35 +0100

    add timestamp to model

--- core/models.py
+++ core/models.py
@@ -14,6 +14,7 @@ class ToDo(models.Model):
 
     name = models.CharField(max_length=255)
     status = models.CharField(choices=STATUS_CHOICES, default="todo", max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
