f8206fcc7cff0045c74c782149cf67fba207ba0e
Alice Campkin <alice.campkin@gmail.com>
2022-07-17 15:27:53 +0100

    adds fake data

---
+++ scripts/create_fake_data.py
@@ -0,0 +1,18 @@
+# exec(open("scripts/create_fake_data.py").read())
+import random
+
+from faker import Faker
+from core.models import ToDo
+
+fake: Faker = Faker()
+
+for i in range(100):
+    todo = ToDo.objects.create(
+        name=fake.paragraph(nb_sentences=1),
+        status=random.choice(ToDo.STATUS_CHOICES)[0],
+    )
+    print(f"Created todo. Name: {todo.name}  Status: {todo.status}")
+
+todo_count = ToDo.objects.count()
+
+print(f"There are {todo_count} todos in the database")
