a6ee6b2994d7d5b847d5335bedddddeb175e04b2
Alice Campkin <alice.campkin@gmail.com>
2022-07-19 23:53:40 +0100

    jazz up UI

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,20 @@
-from django.forms import ModelForm
+from django import forms
 
 from core.models import ToDo
 
 
-class ToDoForm(ModelForm):
+class ToDoForm(forms.ModelForm):
     class Meta:
         model = ToDo
         fields = ["name"]
+
+    name = forms.CharField(
+        label=False,
+        widget=forms.TextInput(
+            attrs={
+                "aria-label": "name",
+                "placeholder": "What do you need to do?",
+                "class": "form-control form-control-lg inline-block",
+            }
+        ),
+    )
